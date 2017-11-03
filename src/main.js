4ca34e43cfca9b54107fe10fc0bffeb7b35a46ba
vsdeveloper <liulongbin1314@outlook.com>
2017-11-03 16:24:52 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// 入口文件
+import Vue from 'vue'
+ 
+
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+
+
+// 按需导入 Mint-UI 中的组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+
+
+// 导入 App 根组件
+import app from './App.vue'
+
+var vm = new Vue({
+  el: '#app',
+  render: c => c(app)
+})
